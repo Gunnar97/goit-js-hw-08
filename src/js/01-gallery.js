33adeb325a928ce542611da214632e7c608e1264
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-14 15:08:25 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description}) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryEl);
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    fadeSpeed:800,
+    captionsData: 'alt',
+    signmentPosition: 'bottom',
+    signmentDelay: 250
+});
