9bf619244d9a46293098372f991d8cb14311d459
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-15 16:34:25 +0300

    add 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorage from './storage.js';
 
+const iframe = document.querySelector('iframe');
+    
+const player = new Player(iframe);
+
+// player.setColor('#ff0000');
+// player.setVolume(0.75);
+
+
+
+function checkTimeForPlay(data) {
+   localStorage.save('videoplayer-current-time', data.seconds); 
+};
+
+player.on('timeupdate', throttle(checkTimeForPlay, 1000));
+player.setCurrentTime(localStorage.load('videoplayer-current-time'));
+
+
+
+
+   
