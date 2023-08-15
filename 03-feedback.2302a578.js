591f74a3d201461850715035047e810444314879
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-15 17:21:28 +0000

    Deploying to gh-pages from @ Gunnar97/goit-js-hw-08@be23fedd73e7945c3ad73207d25c6352142541bc 🚀

--- 03-feedback.2302a578.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),emeil:document.querySelector("input"),textarea:document.querySelector("textarea"),submit:document.querySelector("button")};e.form.addEventListener("submit",function(e){let t={},o=new FormData(e.target.value);o.forEach((e,o)=>{t[o]=e}),console.log(t)});
-//# sourceMappingURL=03-feedback.2302a578.js.map
